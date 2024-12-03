2fdf44e7affd2d9942411c35eaf54ba28e1ab1d9
Shubham Shree <shubhamshree.dev@gmail.com>
2024-12-03 10:58:19 +0530

    indexing bugs fixes

--- app/insights/page.js
+++ app/insights/page.js
@@ -5,9 +5,9 @@ import Insights from "@/components/Insights/Insights"
 
 // export function generateMetadata() {
 //   return {
-    // title: "Blogs and insights on sustainable impact",
-    // description:
-    //   "Our blogs detail our various partnerships that facilitate sustainable change",
+// title: "Blogs and insights on sustainable impact",
+// description:
+//   "Our blogs detail our various partnerships that facilitate sustainable change",
 //     metadataBase: new URL("https://cms.org.in/insights"),
 //     openGraph: {
 //       url: "https://cms.org.in/insights",
@@ -33,13 +33,13 @@ const page = () => {
       "Our blogs detail our various partnerships that facilitate sustainable change",
     path: "insights",
     metaImage: "/social.png",
-    pageType: "WebSite"
-  };
+    pageType: "WebSite",
+  }
   return (
     <>
       <div className="bg-[url('https://s3.ap-south-1.amazonaws.com/com.cg.ivista.mediafiles/cms-origin/cms-website/public/insights/banner.png')] flex flex-col justify-center md:justify-end h-[420px] md:h-[480px] bg-cover bg-center py-10 md:py-16">
-      <Seo {...seoField}/>
-       <Header />
+        <Seo {...seoField} />
+        <Header />
         <div className="ps-2 md:ps-24">
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold text-white text-center sm:text-left">
             Insights
