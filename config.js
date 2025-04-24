30b582935fda34b6e006359b5dec32e9495a5c11
Shubham Shree <shubhamshree.dev@gmail.com>
2025-04-24 10:50:32 +0530

    Testing: Production & Staging

--- config.js
+++ config.js
@@ -5,10 +5,8 @@ const config = {
   LIVE_PRODUCTION_SERVER_ID: "90",
   STAG_PRODUCTION_SERVER_ID: "89",
 
-  // LIVE_SITE_URL: "https://cms.org.in/",
-  // LIVE_SITE_URL_WWW: "https://www.cms.org.in/",
-  LIVE_SITE_URL: "https://staging.cms.org.in/",
-  // LIVE_SITE_URL_WWW: "https://www.cms.org.in/",
+  LIVE_SITE_URL: "https://cms.org.in/",
+  LIVE_SITE_URL_WWW: "https://www.cms.org.in/",
   STAGING_SITE_URL: "https://staging.cms.org.in/",
   LOCAL_SITE_URL: "http://localhost:3020/",
 }
